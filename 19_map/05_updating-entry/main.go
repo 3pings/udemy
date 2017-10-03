bd63edbea9734d9236d4013feeb50ec418633ef7
jbarksdale@threepings.com <jbarksdale@threepings.com>
2017-10-03 19:39:38 -0400

    Completed through section 19

---
+++ 19_map/05_updating-entry/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	myGreeting := map[string]string{
+		"Tim":		"Good Morning",
+		"Jenny": 	"Bonjour",
+	}
+
+	myGreeting["Harleen"] = "Howdy"
+	fmt.Println(myGreeting["Harleen"])
+	myGreeting["Harleen"] = "Gidday"
+	fmt.Println(myGreeting["Harleen"])
+}
---
+++ 19_map/07_comma-ok-idiom_val-exists/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	myGreeting := map[string]string{
+		"Tim":		"Good Morning",
+		"Jenny": 	"Bonjour",
+	}
+
+	myGreeting["Harleen"] = "Howdy"
+	fmt.Println(myGreeting)
+	delete(myGreeting, "Harleen")
+	fmt.Println(myGreeting)
+
+	if val, exists := myGreeting["Harleen"]; exists {
+		fmt.Println("That value Exists!")
+		fmt.Println("val: ", val)
+		fmt.Println("exists: ", exists)
+	} else {
+		fmt.Println("That value does not exists")
+		fmt.Println("val: ", val)
+		fmt.Println("exists: ", exists)
+
+
+	}
+}
